1a8e1c875f248a7ff6fdc191bed2844a4a7879af
Horváth Balázs <balazs630@icloud.com>
2018-07-14 23:06:15 +0200

    Add base project files and settings

---
+++ SkateBudapest/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  SkateBudapest
+//
+//  Created by Horváth Balázs on 2018. 07. 14..
+//  Copyright © 2018. Horváth Balázs. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        initUserDefaults()
+        return true
+    }
+}
+
+// MARK: - UserDefaults setup
+extension AppDelegate {
+    private func initUserDefaults() {
+        let defaults = UserDefaults.standard
+
+        if defaults.object(forKey: UserDefaults.Key.isAppAlreadyLaunchedOnce) == nil {
+            let firstTimeLaunchDefaults: [String: Any] = [
+                UserDefaults.Key.isAppAlreadyLaunchedOnce: true,
+                UserDefaults.Key.appVersion: UpdateService.getCurrentAppVersion()
+            ]
+
+            for item in firstTimeLaunchDefaults {
+                defaults.set(item.value, forKey: item.key)
+            }
+
+            defaults.synchronize()
+        }
+    }
+}
---
+++ SkateBudapest/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SkateBudapest
+//
+//  Created by Horváth Balázs on 2018. 07. 14..
+//  Copyright © 2018. Horváth Balázs. All rights reserved.
+//
+
+import UIKit
+
+struct Constant {
+    static let shortVersionString = "CFBundleShortVersionString"
+}
+
+extension UserDefaults {
+    struct Key {
+        static let isAppAlreadyLaunchedOnce = "isAppAlreadyLaunchedOnce"
+        static let appVersion = "appVersion"
+    }
+}
