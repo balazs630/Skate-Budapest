1290678823bd7758e506fc349e017f94b51b45c6
Horváth Balázs <balazs630@icloud.com>
2018-10-24 23:18:29 +0200

    Open turn-by-turn navigation in Apple Maps

--- SkateBudapest/App flow/LocationDetails/DetailsScreen/RoutingViewController.swift
+++ SkateBudapest/App flow/LocationDetails/DetailsScreen/RoutingViewController.swift
@@ -14,18 +14,47 @@ class RoutingViewController: UIViewController {
     var currentLocation: CLLocationCoordinate2D!
     var destinationLocation: CLLocationCoordinate2D!
 
-    // MARK: Outlets
-    @IBOutlet weak var publicTransportRoutingButton: UIButton!
-    @IBOutlet weak var carRoutingButton: UIButton!
-    @IBOutlet weak var walkRoutingButton: UIButton!
+    var currentLocationMapItem: MKMapItem {
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: currentLocation))
+        mapItem.name = "Source"
 
-    @IBOutlet weak var publicTransportActivityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var carActivityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var walkActivityIndicator: UIActivityIndicatorView!
+        return mapItem
+    }
+
+    var destinationLocationMapItem: MKMapItem {
+        let mapItem =  MKMapItem(placemark: MKPlacemark(coordinate: destinationLocation))
+        mapItem.name = "Destination"
+
+        return mapItem
+    }
+
+    // MARK: Outlets
+    @IBOutlet weak var transitRouteButton: UIButton!
+    @IBOutlet weak var drivingRouteButton: UIButton!
+    @IBOutlet weak var walkingRouteButton: UIButton!
+
+    @IBOutlet weak var transitActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var drivingActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var walkingActivityIndicator: UIActivityIndicatorView!
 
     // MARK: View lifecycle
     override func viewDidAppear(_ animated: Bool) {
         requestETA(for: [.transit, .automobile, .walking])
+    }
+}
+
+// MARK: Actions
+extension RoutingViewController {
+    @IBAction func transitButtonTap(_ sender: Any) {
+        openMaps(directionMode: MKLaunchOptionsDirectionsModeTransit)
+    }
+
+    @IBAction func drivingButtonTap(_ sender: Any) {
+        openMaps(directionMode: MKLaunchOptionsDirectionsModeDriving)
+    }
+
+    @IBAction func walkingButtonTap(_ sender: Any) {
+        openMaps(directionMode: MKLaunchOptionsDirectionsModeWalking)
     }
 }
 
@@ -50,15 +79,9 @@ extension RoutingViewController {
     }
 
     private func prepareTransitRequestInformations() -> MKDirections.Request {
-        let sourceItem = MKMapItem(placemark: MKPlacemark(coordinate: currentLocation,
-                                                          addressDictionary: nil))
-
-        let destinationItem = MKMapItem(placemark: MKPlacemark(coordinate: destinationLocation,
-                                                               addressDictionary: nil))
-
         let request = MKDirections.Request()
-        request.source = sourceItem
-        request.destination = destinationItem
+        request.source = currentLocationMapItem
+        request.destination = destinationLocationMapItem
         request.requestsAlternateRoutes = false
 
         return request
@@ -67,16 +90,21 @@ extension RoutingViewController {
     private func setTransitTimeButtonTexts(for transportType: MKDirectionsTransportType, travelTime: TimeInterval) {
         switch transportType {
         case .transit:
-            publicTransportRoutingButton.setTitle(travelTime.stringTime, for: .normal)
-            publicTransportActivityIndicator.isHidden = true
+            transitRouteButton.setTitle(travelTime.stringTime, for: .normal)
+            transitActivityIndicator.isHidden = true
         case .automobile:
-            carRoutingButton.setTitle(travelTime.stringTime, for: .normal)
-            carActivityIndicator.isHidden = true
+            drivingRouteButton.setTitle(travelTime.stringTime, for: .normal)
+            drivingActivityIndicator.isHidden = true
         case .walking:
-            walkRoutingButton.setTitle(travelTime.stringTime, for: .normal)
-            walkActivityIndicator.isHidden = true
+            walkingRouteButton.setTitle(travelTime.stringTime, for: .normal)
+            walkingActivityIndicator.isHidden = true
         default:
             debugPrint("Unknown transportType: \(transportType)")
         }
+    }
+
+    private func openMaps(directionMode: String) {
+        MKMapItem.openMaps(with: [currentLocationMapItem, destinationLocationMapItem],
+                           launchOptions: [MKLaunchOptionsDirectionsModeKey: directionMode])
     }
 }
